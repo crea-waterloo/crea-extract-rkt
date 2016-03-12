6cf478a90cd9ce7c7fc438d154039c513d1f9512
David Song <david.song0802@gmail.com>
2016-03-12 05:18:06 -0500

    added racket extraction: noun tokenizer

---
+++ extract.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+;'(
+;  (NP
+;   (NP (VBN Activated)
+;       (NNP CD)
+;       (CD 3+)
+;       (JJ enriched)
+;       (JJ human)
+;       (JJ peripheral)
+;       (NN blood)
+;       (NN T)
+;       (NNS cells))
+;   (VP (VBD exhibited)
+;       (NP (NP (JJ potent)
+;               (NN capacity))
+;           (PP (IN for)
+;               (NP (JJ transendothelial)
+;                   (NN migration))))
+;       (PP (IN through)
+;           (NP (NNP HUVEC)
+;               (NNS layers)))
+;       (PP (IN in)
+;           (NP (NP (DT the)
+;                   (NN absence))
+;               (PP (IN of)
+;                   (NP (NNP T)
+;                       (NN cell))))))
+;   (X (SYM ***))) )
+;
+;"Activated CD 3+ enriched human peripheral blood T cells exhibited potent capacity for transendothelial migration through HUVEC 
+;layers in the absence of T cell ***."
+
+(define-struct noun (name order) #:transparent)
+
+(define (tokenize-nouns parse-tree [count 0])
+  (letrec ([tokenize (lambda (parse-tree) 
+                       (cond
+                         [(empty? parse-tree) empty]
+                         [(empty? (rest parse-tree)) empty]
+                         [(list? (second parse-tree)) (append (tokenize (second parse-tree))
+                                                              (tokenize (rest parse-tree)))]
+                         [(or (equal? (first parse-tree) 'NN)
+                              (equal? (first parse-tree) 'NNP)
+                              (equal? (first parse-tree) 'NNS)) (set! count (add1 count))
+                                                                (cons (make-noun (symbol->string (second parse-tree)) count)
+                                                                      (tokenize (rest parse-tree)))]
+                         [else (set! count (add1 count))
+                               (tokenize-nouns (rest parse-tree))]))])
+    (tokenize parse-tree)))
+
+(define (tokenize-input)
+  (define tree (read))
+  (if (eof-object? tree)
+      (void)
+      (begin
+        (printf "~a~n"
+         (tokenize-nouns (first tree)))
+        (tokenize-input))))
+
+(tokenize-input)
