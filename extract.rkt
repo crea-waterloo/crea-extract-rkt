2258e35f8054d52da022afa1ce7b3d4d0ae8740d
David Song <david.song0802@gmail.com>
2016-03-14 05:11:31 -0400

    fixed symbol bug

--- extract.rkt
+++ extract.rkt
@@ -1,36 +1,5 @@
 #lang racket
 
-;'(
-;  (NP
-;   (NP (VBN Activated)
-;       (NNP CD)
-;       (CD 3+)
-;       (JJ enriched)
-;       (JJ human)
-;       (JJ peripheral)
-;       (NN blood)
-;       (NN T)
-;       (NNS cells))
-;   (VP (VBD exhibited)
-;       (NP (NP (JJ potent)
-;               (NN capacity))
-;           (PP (IN for)
-;               (NP (JJ transendothelial)
-;                   (NN migration))))
-;       (PP (IN through)
-;           (NP (NNP HUVEC)
-;               (NNS layers)))
-;       (PP (IN in)
-;           (NP (NP (DT the)
-;                   (NN absence))
-;               (PP (IN of)
-;                   (NP (NNP T)
-;                       (NN cell))))))
-;   (X (SYM ***))) )
-;
-;"Activated CD 3+ enriched human peripheral blood T cells exhibited potent capacity for transendothelial migration through HUVEC 
-;layers in the absence of T cell ***."
-
 (define-struct noun (name order) #:transparent)
 
 (define (tokenize-nouns parse-tree [count 0])
@@ -43,7 +12,9 @@
                          [(or (equal? (first parse-tree) 'NN)
                               (equal? (first parse-tree) 'NNP)
                               (equal? (first parse-tree) 'NNS)) (set! count (add1 count))
-                                                                (cons (make-noun (symbol->string (second parse-tree)) count)
+                                                                (cons (make-noun (if (symbol? (second parse-tree))
+                                                                                     (symbol->string (second parse-tree))
+                                                                                     (second parse-tree)) count)
                                                                       (tokenize (rest parse-tree)))]
                          [else (set! count (add1 count))
                                (tokenize-nouns (rest parse-tree))]))])
